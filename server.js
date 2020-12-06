be0bea05759d27c5bfc7149df5d4c67b502edfcf
Jin Choo <jinchoo315@gmail.com>
2020-12-05 20:33:39 -0600

    add/delet to api and html route files. Need to add screenshots and description of the projects.

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 // Series of npm packages that we will use to give our server useful functionality
 
-var express = require("express");
-var fs = require("fs");
+const express = require("express");
+const fs = require("fs");
+
 // Tells node that we are creating an "express" server
-var app = express();
+const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
-// app.use(express.json());
 
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
 // require("./public/index.html")(app);
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/api")(app);
+require("./routes/html")(app);
 
 // The below code effectively "starts" our server
 
